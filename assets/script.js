a100f38ea9f3a7ce7a1bde4cc451e0f6eaaa9013
GIIIVENS <giiivensart@gmail.com>
2022-05-05 16:55:52 -0500

    finally got the questions to appear

--- assets/script.js
+++ assets/script.js
@@ -1,149 +1,253 @@
 
+// variables 
 
 const start = document.getElementById('start-btn')
-let welcome = document.getElementById('welcome')
+
 const options = document.getElementById('question-box')
-const question = document.getElementById('#questions')
-let answer = document.getElementById('#answers-btn') 
-let   questionOrdr, questionUserIsOn
+const questionTextEl = document.getElementById('question-text')
+// const userResult = document.getElementById('')
+// const restart = document.getElementById('')
+let welcome = document.getElementById('welcome')
+let answer = document.getElementById('#answers-btn')
+let questionOrdr, questionUserIsOn
+let score = 0
+let userCurrentQuestion = {}
+let correctAnswer = true
+let questionNumber = 0
+// buttons
+start.addEventListener('click', quizBegin)
+// answer.addEventListener('click', answerbtn)
+
+
+const quizQuestions = [
+    {
+        question: 'Which of the following is NOT a JavaScript data type? ',
+        answers: [
+            {
+                a: 'object',
+                b: 'boolean',
+                c: 'cascade',
+                c: 'string'
+            },
+            { correct: "C" }
+
+        ]
+    },
+
+    {
+        question: 'Is JavaScript case sensitive?',
+        answers: [
+            {
+                a: 'no',
+                b: 'yes',
+                c: 'sometimes',
+                d: 'only on tuesdays',
+            },
+            { correct: "b" }
+        ]
+
+    },
+
+
+    {
+        question: 'Which of the following is NOT a way JavaScript code can correspond with an HTML file?',
+        answers: [
+            {
+                a: 'inline',
+                b: 'outline',
+                c: 'internal',
+                d: 'external'
+            },
+            { correct: "b" }
+        ]
+
+    },
+
+    {
+        question: ' What kind of storage in JavaScript gets cleared when the page session ends?',
+        answers: [
+            {
+                a: 'browser storage',
+                b: 'local storage',
+                c: 'script storage',
+                d: 'session storage'
+            },
+            { correct: "d" }
+        ]
+
+    },
+
+    {
+        question: 'In JavaScript what does DOM stand for? ',
+        answers: [
+            {
+                a: 'Document Object Model',
+                b: 'Direct Object Management',
+                c: 'Declare Object Material',
+                d: 'Document Object Metadata '
+            },
+            { correct: "a" }
+        ]
+
+    },
+
+    {
+        question: 'which of the following is NOT a javascript data type ',
+        answers: [
+            {
+                a: 'object',
+                b: 'boolean',
+                c: 'cascade',
+                d: 'string'
+            },
+            { correct: "c" }
+        ]
+
+    },
+
+    {
+        questions: 'where should an external Javasript page be linked in the HTML? ',
+        answers: [
+            {
+                a: 'at the top of the document',
+                b: 'at the bottom of the document',
+                c: 'anywhere in the document',
+                d: 'in a media query'
+            },
+            { correct: "b" }
+        ]
+
+    }
+];
 
 
 
 
-
-start.addEventListener('click',quizBegin)
-
-function instructionToggle (){
+function instructionToggle() {
     var welcome = document.getElementById('welcome')
-    if (welcome.style.display ==="block") {
+    if (welcome.style.display === "block") {
         welcome.style.display = "none";
     } else {
-       welcome.style.display= 'block';
+        welcome.style.display = 'block';
     }
 
 }
-// function textToggle 
+
+
+
+
+
+
+
+
+// function textToggle text off once one clicks start quiz begins
+
+
+
+
 
 function quizBegin() {
     console.log('quizStart')
     start.classList.add('hide')
     welcome.classList.add('hide')
     options.classList.remove('hide')
-    questionOrdr = questions.sort(()=> Math.random)
-    questionUserIsOn = 0
-    nextQuestion ()
-}   
-// funtion that begins the quiz after the start btn is selected 
-
-
-
-
-function nextQuestion () { 
-    presentQuestion(questionOrdr[questionUserIsOn]) 
+    // nextQuestion()
+    loadQuestion()
 
 }
-// function that 
- 
-function presentQuestion (question) {
-    question. questions.question 
-    question.answer.array.forEach(answer=> {
-        const answerbutton = document.createElement('buttn')
-        buttn.innerText= answer.Text
-        
-    });
+
+
+function loadQuestion(){
+ const currentQuestion = quizQuestions[0]
+console.log (currentQuestion.question)
+console.log (questionTextEl)
+questionTextEl.innerText = currentQuestion.question
 }
 
-const quizQuestion = [
-    { question: 'Which of the following is NOT a JavaScript data type? ',
-        answers: [
-            { Text: 'object', correct: false},
-            { Text: 'boolean', correct: false},
-            { Text: 'cascade', correct: true},
-            { Text: 'string', correct: false},
 
-        ]
-    } ,
 
-    {
-        question: 'Is JavaScript case sensitive?',
-        answers: [
-            { Text: 'no', correct: false},
-            { Text: 'yes', correct: true},
-            { Text: 'sometimes', correct: false},
-            { Text: 'only on tuesdays', correct: false},
 
-        ]
-        
-    },
-    
-    
-      {
-        question: 'Which of the following is NOT a way JavaScript code can correspond with an HTML file?',
-        answers: [
-            { Text: 'inline', correct: false},
-            { Text: 'outline', correct: true},
-            { Text: 'internal', correct: false},
-            { Text: 'external', correct: false},
 
-        ]
-        
-    },
+nextQuestion = () => {
+    if (totalOfQuestions.length === 0 || scoreTotal > full) {
+        localStorage.setItem('topScores', score)
 
-    {
-        question: ' What kind of storage in JavaScript gets cleared when the page session ends?',
-        answers: [
-            { Text: 'browser storage', correct: false},
-            { Text: 'local storage', correct: false},
-            { Text: 'script storage', correct: false},
-            { Text: 'session storage', correct: true},
-
-        ]
-        
-    },
-
-    {
-        question: 'In JavaScript what does DOM stand for? ',
-        answers: [
-            { Text: 'Document Object Model', correct: true},
-            { Text: 'Direct Object Management', correct: false},
-            { Text: 'Declare Object Material', correct: false},
-            { Text: 'Document Object Metadata ', correct: false},
-
-        ]
-        
-    },
-
-    {
-        question: 'which of the following is NOT a javascript data type ',
-        answers: [
-            { Text: 'object', correct: false},
-            { Text: 'boolean', correct: false},
-            { Text: 'cascade', correct: true},
-            { Text: 'string', correct: false},
-
-        ]
-        
-    },
-
-    {
-        questions: 'where should an external Javasript page be linked in the HTML? ',
-        answers: [
-            { Text: 'at the top of the document', correct: false},
-            { Text: 'at the bottom of the document', correct: true},
-            { Text: 'anywhere in the document', correct: false},
-            { Text: 'in a media query', correct: false},
-
-        ]
-        
+        return window.location.assign('end/.html')
     }
 
+    scoreTotal++
+    progressText.innerTex = 'Question ${scoreTotal} of ${scoreTotal}'
+    Math.floor(Math.random() * totalOfQuestions.length)
+    userCurrentQuestion = totalOfQuestions[questionsIndex]
+    question.innerText = userCurrentQuestion.quizQuestions
 
 
-]
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = userCurrentQuestion['choice' + number]
+
+        // availbleQuestions.questionsIndex, 1
+
+        acceptingAnswers = true
 
 
 
- 
+
+
+
+    })
+}
+
+
+
+
+
+
+//  questionOrdr = questions.sort(()=> Math.random)
+// funtion that begins the quiz after the start btn is selected 
+function nextQuestion() {
+    currentQuestion = 0;
+    questionText.innerhtml = questions[currentQuestion].question;
+
+
+
+}
+
+function answerbtn() {
+
+
+
+
+
+
+}
+// // function that 
+
+function presentQuestion(question) {
+    question.answer.array.forEach(answer => {
+        const answerbutton = document.createElement('buttn')
+        buttn.innerText = answer.Text
+    })
+}
+//    
+// 
+
+
+
+// quiz questions and answers
+
+
+
+function submitResults() {
+
+}
+
+function seeResults() {
+
+}
+
 // function tah makes new question appear after the  previous question has been answered 
 
+const scoreTotal = 35
+const totalOfQuestions = 7
 
